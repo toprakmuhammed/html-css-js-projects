131206cdbdb4d1cad4018667c7a862423e969c2d
Muhammed Toprak <muhammedtoprak@live.com>
2025-09-16 23:26:19 +0300

    add base DOM references.

---
+++ project-18-currency-converter/script.js
@@ -0,0 +1,5 @@
+const converterForm = document.getElementById("converter-form");
+const fromCurrency = document.getElementById("from-currency");
+const toCurrency = document.getElementById("to-currency");
+const amountInput = document.getElementById("amount");
+const resultDiv = document.getElementById("result");
