962cb9ded980e629bb606d20bd311d340ca2444e
Muhammed Toprak <muhammedtoprak@live.com>
2025-10-17 23:56:55 +0300

    javascript building.

---
+++ project-19-student-management-system/script.js
@@ -0,0 +1,7 @@
+//(mock data)
+let lectures = [
+  { name: "Web Development", students: 12, gpa: 72.3 },
+  { name: "Database", students: 8, gpa: 81.0 },
+  { name: "Cyber Security", students: 10, gpa: 76.4 },
+  { name: "Machine Learning", students: 9, gpa: 79.1 },
+];
